6779f792ca66768c6bfdb222d440838a7942c024
unknown <david.caceres.efisat@gmail.com>
2020-02-01 23:53:33 +0100

    Tutorial

---
+++ Assets/SpanEnemy.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class SpanEnemy : MonoBehaviour
+{
+   public Transform tr;
+   public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    void LateUpate() {
+0        transform.position = tr.position + offset;
+    }
+
+}
